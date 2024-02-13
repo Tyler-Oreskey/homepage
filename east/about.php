f80b16a69de07bb436dd4d75276e3abe1f322cd7
Tyler-Oreskey <toreskey@gmail.com>
2024-02-13 22:02:16 +0000

    moved profile card to about me.

--- east/about.php
+++ east/about.php
@@ -8,6 +8,10 @@
       <title>About</title>
       <link rel="stylesheet" href="./styles/global.css">
       <link rel="stylesheet" href="./styles/about.css">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+      <link rel="preconnect" href="https://fonts.googleapis.com">
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+      <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&family=Raleway:ital,wght@0,100..900;1,100..900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
     </head>
   <body>
     <nav>
@@ -28,6 +32,33 @@
         </h2>
       </header>
       <section>
+      <div class="card">
+            <div class="text">
+                <header>
+                    <h1 class="name"><span>Tyler</span> Oreskey</h1>
+                    <h2>My <span>Passion</span> and <span>Focus</span> is Software Engineering</h2>
+                </header>
+                <p>
+                    Work in the software development field on challenging and interesting projects. 
+                    Ideally, utilizing my skills and passion for analyzing data and rapidly 
+                    creating robust valuable web solutions driving customer adoption and increased 
+                    profit.
+                </p>
+                <div class="social-icons">
+                    <a href="https://www.linkedin.com/in/tyler-oreskey/" target="_blank">
+                        <i class="fa-brands fa-linkedin fa-2x"></i>
+                    </a>
+                    <a href="https://github.com/Tyler-Oreskey" target="_blank">
+                        <i class="fa-brands fa-github fa-2x"></i>
+                    </a>
+                    <a href="mailto:toreskey@gmail.com" target="_blank">
+                        <i class="fa-solid fa-envelope fa-2x"></i>
+                    </a>
+                </div>
+            </div>
+            <img class="image" src="../images/Cartoon Generator.png" alt="photo of me holding a fish.">
+        </div>
+
       <div class="card">
             <div class="text">
                 <p>
