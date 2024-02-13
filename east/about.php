0f78a4d4c53638e0b1e1aed6d695852b7148d834
Tyler-Oreskey <toreskey@gmail.com>
2024-02-13 21:51:17 +0000

    added content for about me page. added fonts and styles.

--- east/about.php
+++ east/about.php
@@ -18,24 +18,82 @@
           <li><a href="./work.php">Work</a></li>
       </ul>
     </nav>
-    <header>
-      <h1>About</h1>
-      <h2>
-        A CSU student studying Computer Science and Software Engineering 
-        with a desire to become a full stack engineer. I enjoy fly 
-        fishing, the gym, videogames, and visiting breweries.
-      </h2>
-    </header>
-    <section>
-      <img src="../images/guide.JPG" width="200px">
-      <img src="../images/af.png" width="200px">
-      <img src="../images/angel.jpg" width="200px">
-      <img src="../images/cs2.png" width="200px">
-      <img src="../images/dad.jpg" width="200px">
-      <img src="../images/nick1.PNG" width="200px">
-      <img src="../images/nick2.JPG" width="200px">
-      <img src="../images/voodoo.png" width="200px">
-    </section>
+    <main>
+      <header>
+        <h1>About</h1>
+        <h2>
+          A CSU student studying Computer Science and Software Engineering 
+          with a desire to become a full stack engineer. I enjoy fly 
+          fishing, the gym, videogames, and visiting breweries.
+        </h2>
+      </header>
+      <section>
+      <div class="card">
+            <div class="text">
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+                </p>
+            </div>
+            <img src="../images/guide.JPG" alt="Fishing Guide Taking a photo with my fish.">
+        </div>
+        <div class="card">
+            <div class="text">
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+                </p>
+            </div>
+            <img src="../images/af.png" alt="Anytime Fitness logo.">
+        </div>
+        <div class="card">
+            <div class="text">
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+                </p>
+            </div>
+            <img src="../images/angel.jpg" alt="My dog sleeping.">
+        </div>
+        <div class="card">
+            <div class="text">
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+                </p>
+            </div>
+            <img src="../images/cs2.png" alt="Counter Strike 2 logo.">
+        </div>
+        <div class="card">
+            <div class="text">
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+                </p>
+            </div>
+            <img src="../images/dad.jpg" alt="My dad holding a caught fish.">
+        </div>
+        <div class="card">
+            <div class="text">
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+                </p>
+            </div>
+            <img src="../images/nick1.PNG" alt="My friend holding a caught fish.">
+        </div>
+        <div class="card">
+            <div class="text">
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+                </p>
+            </div>
+            <img src="../images/nick2.JPG" alt="My friend holding a caught fish.">
+        </div>
+        <div class="card">
+            <div class="text">
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+                </p>
+            </div>
+            <img src="../images/voodoo.png" alt="New Belgium's Voodoo Ranger IPA logo.">
+        </div>
+      </section>
+    </main>
     <footer>
       <p>Tyler Oreskey</p>
     </footer>
