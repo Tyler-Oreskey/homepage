e7412f719298bb0ab0e85ef303c04081b0c7c0c0
Tyler-Oreskey <toreskey@gmail.com>
2024-02-13 07:37:10 +0000

    picked theme and tweaked styles for navbar.

--- east/index.php
+++ east/index.php
@@ -8,6 +8,7 @@
         <title>East</title>
         <link rel="stylesheet" href="./styles/global.css">
         <link rel="stylesheet" href="./styles/index.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
     </head>
     <body>
         <nav>
@@ -23,7 +24,7 @@
             <div class="text">
                 <header>
                     <h1 class="name"><span>Tyler</span> Oreskey</h1>
-                    <h2>My passion and focus is Software Engineering</h2>
+                    <h2>My <span>Passion</span> and <span>Focus</span> is Software Engineering</h2>
                 </header>
                 <p>
                     Work in the software development field on challenging and interesting projects. 
@@ -31,6 +32,17 @@
                     creating robust valuable web solutions driving customer adoption and increased 
                     profit.
                 </p>
+                <div class="social-icons">
+                    <a href="https://www.linkedin.com/in/tyler-oreskey/" target="_blank">
+                        <i class="fa-brands fa-linkedin fa-2x"></i>
+                    </a>
+                    <a href="https://github.com/Tyler-Oreskey" target="_blank">
+                        <i class="fa-brands fa-github fa-2x"></i>
+                    </a>
+                    <a href="mailto:toreskey@gmail.com" target="_blank">
+                        <i class="fa-solid fa-envelope fa-2x"></i>
+                    </a>
+                </div>
             </div>
             <img class="image" src="../images/Cartoon Generator.png">
         </div>
