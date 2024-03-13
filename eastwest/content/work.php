c7317f3bc887165b6b84db43f3b831956e3dae3d
tyler-oreskey <toreskey@gmail.com>
2024-03-13 01:29:59 +0000

    moved images and styles folder into content

--- eastwest/content/work.php
+++ eastwest/content/work.php
@@ -1,47 +1,28 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8">
-    <meta name="description" content="Work">
-    <meta name="keywords" content="Tyler Oreskey Previous Work">
-    <meta name="author" content="Tyler Oreskey">
-    <title>Work</title>
-    <link rel="stylesheet" href="./styles/global.css">
-    <link rel="stylesheet" href="./styles/work.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&family=Raleway:ital,wght@0,100..900;1,100..900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
-  </head>
-  <body>
-    <main>
+<main>
       <header><h1>Previous Work</h1></header>
-          <div class="container">
+        <div class="container">
             <div class="card">
-                <img src="images/toi.png" alt="TapOnIt logo.">
+                <img src="content/images/toi.png" alt="TapOnIt logo.">
                 <p><span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor</span></p>
                 <div class="button"><a href="https://taponit.com/" target="_blank">Visit</a></div>
             </div>
 
             <div class="card">
-                <img src="images/scheels.png" alt="Scheels logo.">
+                <img src="content/images/scheels.png" alt="Scheels logo.">
                 <p><span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor</span></p>
                 <div class="button"><a href="https://scheels.com/" target="_blank">Visit</a></div>
             </div>
 
             <div class="card">
-                <img src="images/hach.jpg" alt="Hach logo.">
+                <img src="content/images/hach.jpg" alt="Hach logo.">
                 <p><span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor</span></p>
                 <div class="button"><a href="https://hach.com" target="_blank">Visit</a></div>
             </div>
         </div>
         <p class="filler-text">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Id aliquet lectus proin nibh. Nulla aliquet enim tortor at auctor urna. Non sodales neque sodales ut etiam. Hac habitasse platea dictumst quisque sagittis purus sit. Eget nunc scelerisque viverra mauris in aliquam sem. Feugiat scelerisque varius morbi enim nunc. Elementum curabitur vitae nunc sed velit dignissim sodales ut. Eget gravida cum sociis natoque penatibus et. Pellentesque diam volutpat commodo sed. Diam maecenas sed enim ut sem viverra aliquet. Et tortor consequat id porta nibh venenatis cras. Pellentesque elit ullamcorper dignissim cras tincidunt lobortis feugiat vivamus at. Cras pulvinar mattis nunc sed blandit libero volutpat. Vitae congue eu consequat ac felis donec et odio. Sociis natoque penatibus et magnis dis parturient montes nascetur.
-                </p>
-        <p class="filler-text">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Id aliquet lectus proin nibh. Nulla aliquet enim tortor at auctor urna. Non sodales neque sodales ut etiam. Hac habitasse platea dictumst quisque sagittis purus sit. Eget nunc scelerisque viverra mauris in aliquam sem. Feugiat scelerisque varius morbi enim nunc. Elementum curabitur vitae nunc sed velit dignissim sodales ut. Eget gravida cum sociis natoque penatibus et. Pellentesque diam volutpat commodo sed. Diam maecenas sed enim ut sem viverra aliquet. Et tortor consequat id porta nibh venenatis cras. Pellentesque elit ullamcorper dignissim cras tincidunt lobortis feugiat vivamus at. Cras pulvinar mattis nunc sed blandit libero volutpat. Vitae congue eu consequat ac felis donec et odio. Sociis natoque penatibus et magnis dis parturient montes nascetur.
-                </p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Id aliquet lectus proin nibh. Nulla aliquet enim tortor at auctor urna. Non sodales neque sodales ut etiam. Hac habitasse platea dictumst quisque sagittis purus sit. Eget nunc scelerisque viverra mauris in aliquam sem. Feugiat scelerisque varius morbi enim nunc. Elementum curabitur vitae nunc sed velit dignissim sodales ut. Eget gravida cum sociis natoque penatibus et. Pellentesque diam volutpat commodo sed. Diam maecenas sed enim ut sem viverra aliquet. Et tortor consequat id porta nibh venenatis cras. Pellentesque elit ullamcorper dignissim cras tincidunt lobortis feugiat vivamus at. Cras pulvinar mattis nunc sed blandit libero volutpat. Vitae congue eu consequat ac felis donec et odio. Sociis natoque penatibus et magnis dis parturient montes nascetur.
+            <br>
+            <br>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Id aliquet lectus proin nibh. Nulla aliquet enim tortor at auctor urna. Non sodales neque sodales ut etiam. Hac habitasse platea dictumst quisque sagittis purus sit. Eget nunc scelerisque viverra mauris in aliquam sem. Feugiat scelerisque varius morbi enim nunc. Elementum curabitur vitae nunc sed velit dignissim sodales ut. Eget gravida cum sociis natoque penatibus et. Pellentesque diam volutpat commodo sed. Diam maecenas sed enim ut sem viverra aliquet. Et tortor consequat id porta nibh venenatis cras. Pellentesque elit ullamcorper dignissim cras tincidunt lobortis feugiat vivamus at. Cras pulvinar mattis nunc sed blandit libero volutpat. Vitae congue eu consequat ac felis donec et odio. Sociis natoque penatibus et magnis dis parturient montes nascetur.
+        </p>
     </main>
-    <footer><p>Tyler Oreskey</p></footer>
-  </body>
-</html>
--- eastwest/index.php
+++ eastwest/index.php
@@ -2,11 +2,11 @@
     $direction = isset($_GET['direction']) ? $_GET['direction'] : 'west';
     $page = isset($_POST['page']) ? $_POST['page'] : 'index';
     
-    $globalStyles = "./styles/$direction/global.css";
-    $pageStyles = "./styles/$direction/$page.css";
+    $globalStyles = "content/styles/$direction/global.css";
+    $pageStyles = "content/styles/$direction/$page.css";
     $pageTitle = ucfirst($direction) . ' ' . ucfirst($page);
 
     include "content/header.php";
-    include "./content/nav.php";
-    include "./content/$page.php";
-    include "./content/footer.php";
+    include "content/nav.php";
+    include "content/$page.php";
+    include "content/footer.php";
